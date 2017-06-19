f0155033c86c1bf26098778ebfb1e16d47a63779
Gerald Bauer <gerald.bauer@gmail.com>
2017-06-19 22:03:40 +0200

    up authors

--- script/mkauthors.rb
+++ script/mkauthors.rb
@@ -7,16 +7,27 @@ require 'pp'
 require 'yaml'
 
 
-themes_repo = File.expand_path( '../themes' )
-site_repo   = File.expand_path( '../drjekyllthemes.github.io' )
+require_relative 'settings'    # e.g. THEMES_REPO, SITE_REPO
 
-text   = File.read( "#{themes_repo}/themes.yml" )
+
+themes_repo = THEMES_REPO    ## File.expand_path( '../themes' )
+site_repo   = SITE_REPO      ## File.expand_path( '../drjekyllthemes.github.io' )
+
+
+## read known ("white-listed") authors from site repo
+text    = File.open( "#{site_repo}/_data/authors.yml", 'r:utf-8') { |f| f.read }
+known_authors = YAML.load( text )
+
+pp known_authors
+
+
+text   = File.open( "#{themes_repo}/themes.yml", 'r:utf-8') { |f| f.read }
 themes = YAML.load( text )
 
 pp themes
 
 ## build hash with authors and list of themes
-authors = {}
+all_authors = {}
 
 themes.each do |theme|
   author = theme['author']
@@ -28,23 +39,59 @@ themes.each do |theme|
   ## todo/fix: allow multi authors!! split using comma (,) etc.
   ## for now remove et al
   author = author.sub( 'et al', '' ).strip
-  ary = authors[ author ] || []
+  ary = all_authors[ author ] || []
   ary << theme['title']  ## add theme to theme list/array
-  authors[ author ] = ary
+  all_authors[ author ] = ary
 end
 
-pp authors
+pp all_authors
+
+
+
+### generate authors collection pages
+all_authors.each do |k,v|
+
+  author = known_authors[k]
+  if author
+    puts "   *** bingo!!! found #{k}"
+
+    github = author['github']
+    if github.nil?
+      puts "sorry github handle missing!!!"
+      exit 1
+    end
+
+    front_matter = {
+      'name'   => k,
+      'themes' => v
+    }
+
+    ## File.open( "./o/#{github}.md", 'w:utf-8' ) do |f|
+    File.open( "#{site_repo}/_authors/#{github}.md", 'w:utf-8' ) do |f|
+      f.write YAML.dump( front_matter )
+      f.write "---\n"    ## add end front matter marker too (note: begin gets auto-added)
+    end
+  else
+    puts "!!! warn: author record missing for >#{k}<"
+  end
+end
+
+
+
+
+
 
 ### convert to nice yaml datafile format
 
-authors2 = []
-authors.each do |k,v|
-  authors2 << { 'name' => k, 'themes' => v}  # note: use string as key (for pretty printing yaml output)
+all_authors2 = []
+all_authors.each do |k,v|
+  all_authors2 << { 'name' => k, 'themes' => v}  # note: use string as key (for pretty printing yaml output)
 end
 
-puts YAML.dump( authors2 )
+## puts YAML.dump( all_authors2 )
 
-
-File.open( "#{site_repo}/_data/o/themes/authors.yml", 'w' ) do |f|
-  f.write YAML.dump( authors2 )
+## todo/fix: use utf8 encoding!!!
+## File.open( "./o/authors.yml", 'w:utf-8' ) do |f|
+File.open( "#{site_repo}/_data/o/themes/authors.yml", 'w:utf-8' ) do |f|
+  f.write YAML.dump( all_authors2 )
 end
